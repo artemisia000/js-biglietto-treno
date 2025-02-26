609633dd15864d6e23c55696ca7caaf0dfab9f0f
artemisia000 <misiapierobon@gmail.com>
2025-02-26 15:43:52 +0100

    euro

--- js/main.js
+++ js/main.js
@@ -11,20 +11,20 @@ console.log(age);
 //viaggio per chilometro a 0.21
 const price = viaggio * 0.21;
 console.log(price);
-let message = `Il prezzo del biglietto è ${price.toFixed(2)}`;
+let message = `Il prezzo del biglietto è ${price.toFixed(2)} €`;
 
 //sconto minorenni 20%
 if( age < 18 ){
     calc = price-(price / 100 * 20);
     console.log(calc);
-    message = `Hai diritto allo sconto del 20%.Il totale è: ${calc.toFixed(2)}`;
+    message = `Hai diritto allo sconto del 20%.Il totale è: ${calc.toFixed(2)} €`;
 }
 
 //sconto over 65 40%
 else if( age >= 65 ){
     calc = price-(price / 100 * 40);
     console.log(calc);
-    message = `Hai diritto allo sconto del 40%.Il totale è ${calc.toFixed(2)}`;
+    message = `Hai diritto allo sconto del 40%.Il totale è ${calc.toFixed(2)} €`;
 }
 
 
